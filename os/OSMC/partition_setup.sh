98f8c763b474772d90bba30872e74d292a805ca6
rern <rernrern@gmail.com>
2017-04-01 17:18:37 +0700

    Update partition_setup.sh

--- os/OSMC/partition_setup.sh
+++ os/OSMC/partition_setup.sh
@@ -28,12 +28,12 @@ umount /tmp/mount
 sync
 # Fix the fstab
 mount $part2 /tmp/mount
-echo "$part1  /boot    vfat     defaults,noatime    0   0
-$part2  /    ext4      defaults,noatime    0   0
+echo "$part1          /boot              vfat     defaults,noatime  0   0
+$part2          /                  ext4     defaults,noatime  0   0
 /dev/mmcblk0p1  /media/RECOVERY    vfat     noauto,noatime    0   0
 /dev/mmcblk0p5  /media/SETTINGS    ext4     noauto,noatime    0   0
-/dev/mmcblk0p8  /media/boot     vfat     noauto,noatime    0   0
-/dev/mmcblk0p9  /media/root                  ext4     noauto,noatime    0   0
+/dev/mmcblk0p8  /media/boot        vfat     noauto,noatime    0   0
+/dev/mmcblk0p9  /media/root        ext4     noauto,noatime    0   0
 " > /tmp/mount/etc/fstab
 
 # customize files
