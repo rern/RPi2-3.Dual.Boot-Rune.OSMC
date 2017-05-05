0fb5016f14a299da114d966443b60fd76ce115a8
rern <rernrern@gmail.com>
2017-05-05 20:29:47 +0700

    Update formatusb.sh

--- formatusb.sh
+++ formatusb.sh
@@ -16,18 +16,18 @@ info=$( echo $(tput setab 6; tput setaf 0) i $(tput setab 0; tput setaf 7) )
 # functions #######################################
 
 title2() {
-        echo -e "\n$line2\n"
-        echo -e "$bar $1"
-        echo -e "\n$line2\n"
+    echo -e "\n$line2\n"
+    echo -e "$bar $1"
+    echo -e "\n$line2\n"
 }
 title() {
-        echo -e "\n$line"
-        echo $1
-        echo -e "$line\n"
+    echo -e "\n$line"
+    echo $1
+    echo -e "$line\n"
 }
 titleend() {
-        echo -e "\n$1"
-        echo -e "\n$line\n"
+    echo -e "\n$1"
+    echo -e "\n$line\n"
 }
 
 title "$info Storage list"
