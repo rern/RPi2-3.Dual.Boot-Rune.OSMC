121217f6932aae7fdcbdc73c6fac9bbc58e0aa4b
rern <rernrern@gmail.com>
2017-05-20 18:17:29 +0700

    Update partition_setup.sh

--- os/RuneAudio/partition_setup.sh
+++ os/RuneAudio/partition_setup.sh
@@ -14,4 +14,11 @@ umount /tmp/mount
 mount $part2 /tmp/mount
 sed -i "s|^.* /boot |$part1  /boot |" /tmp/mount/etc/fstab
 cp -r /mnt/os/RuneAudio/custom/. /tmp/mount # customize files
+
+sed -i -e 's/Reboot/Reboot to NOOBS/
+' -e $'/id="reboot"/ i\
+\t\t\t\t<button id="bootosmc" class="btn btn-primary btn-lg btn-block" data-dismiss="modal"><i class="fa fa-refresh"></i> Reboot to OSMC</button>
+\t\t\t\t<button id="bootrune" class="btn btn-primary btn-lg btn-block" data-dismiss="modal"><i class="fa fa-refresh"></i> Reboot to RuneAudio</button>
+' /tmp/mount/srv/http/app/templates/footer.php
+
 umount /tmp/mount
