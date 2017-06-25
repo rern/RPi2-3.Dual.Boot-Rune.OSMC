3ea112f774e12e1f1954b231c3b04434d92710d7
rern <rernrern@gmail.com>
2017-06-25 20:28:01 +0700

    Update rebootosmc.py

--- os/OSMC/custom/home/osmc/rebootosmc.py
+++ os/OSMC/custom/home/osmc/rebootosmc.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 import os
 
-os.system('sudo /home/osmc/gpiooff.py')
+os.system('sudo /home/osmc/gpiooff.py 1')
 os.system('sudo /home/osmc/rebootosmc.sh')
+
+# current raspbian has no /sys/module/bcm2709/parameters/reboot_part > use noobd.conf instead
+#os.system('sudo echo 6 > /sys/module/bcm2709/parameters/reboot_part; /var/www/command/rune_shutdown; reboot')
