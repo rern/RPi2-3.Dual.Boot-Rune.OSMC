9bd31816e52714825955883a835ad5b684728109
rern <rernrern@gmail.com>
2017-06-26 10:27:22 +0700

    Update rebootosmc.sh

--- os/OSMC/custom/home/osmc/rebootosmc.sh
+++ os/OSMC/custom/home/osmc/rebootosmc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p /mnt/p5
-mount /dev/mmcblk0p5 /mnt/p5
-sed -i "s/default_partition_to_boot=./default_partition_to_boot=6/" /mnt/p5/noobs.conf
+mkdir -p /tmp/p5
+mount /dev/mmcblk0p5 /tmp/p5
+sed -i "s/default_partition_to_boot=./default_partition_to_boot=6/" /tmp/p5/noobs.conf
 reboot
