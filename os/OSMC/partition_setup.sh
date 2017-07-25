6afd41b5449351182539fcf4fd04145a71415538
rern <rernrern@gmail.com>
2017-07-25 20:36:52 +0700

    Update partition_setup.sh

--- os/OSMC/partition_setup.sh
+++ os/OSMC/partition_setup.sh
@@ -35,14 +35,14 @@ $part2  /                ext4  defaults,noatime  0    0
 /dev/mmcblk0p8  /media/boot      vfat  noauto,noatime    0    0
 /dev/mmcblk0p9  /media/root      ext4  noauto,noatime    0    0
 /dev/sda1       /mnt/hdd         ext4  defaults,noatime  0    0
-">/tmp/mount/etc/fstab
+" > /tmp/mount/etc/fstab
 
 # customize files
 sed -i "s/root:.*/root:\$6\$X6cgc9tb\$wTTiWttk\/tRwPrM8pLZCZpYpHE8zEar2mkSSQ7brQvflqhA5K1dgcyU8nzX\/.tAImkMbRMR0ex51LjPsIk8gm0:17000:0:99999:7:::/" /tmp/mount/etc/shadow
 sed -i "s/PermitRootLogin without-password/PermitRootLogin yes/" /tmp/mount/etc/ssh/sshd_config
 
 cp -r /mnt/os/OSMC/custom/. /tmp/mount # copy recursive include hidden ('.' not '*')
-chmod 644 /etc/udev/rules.d/usbsound.rules
+chmod 644 /tmp/mount/etc/udev/rules.d/usbsound.rules
 chmod 755 /tmp/mount/home/osmc/*.py
 chown -R 1000:1000 /tmp/mount/home/osmc # 'osmc' dir copied as root before os create - chown needed
 
