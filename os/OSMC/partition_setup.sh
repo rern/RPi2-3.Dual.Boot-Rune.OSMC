2b5ab209b3793870ff79dd657242f274ca45766c
rern <rernrern@gmail.com>
2017-07-28 18:45:56 +0700

    Update partition_setup.sh

--- os/OSMC/partition_setup.sh
+++ os/OSMC/partition_setup.sh
@@ -26,8 +26,7 @@ umount /tmp/mount
 sync
 # Fix the fstab
 mount $part2 /tmp/mount
-echo "# filesystem	dir	             type  options           dump pass
-#------------------------------------------------------------------
+echo "
 $vfat_part /boot            vfat  defaults,noatime  0    0
 /dev/mmcblk0p1  /media/RECOVERY  vfat  noauto,noatime    0    0
 /dev/mmcblk0p5  /media/SETTINGS  ext4  noauto,noatime    0    0
