8be03d5dfa2ad407588b35d9486ed2dbd18f13a8
rern <rernrern@gmail.com>
2017-08-02 21:11:11 +0700

    Update partition_setup.sh

--- os/OSMC/partition_setup.sh
+++ os/OSMC/partition_setup.sh
@@ -38,7 +38,9 @@ $vfat_part  /boot      vfat  defaults,noatime
 
 # customize files
 sed -i "s/root:.*/root:\$6\$X6cgc9tb\$wTTiWttk\/tRwPrM8pLZCZpYpHE8zEar2mkSSQ7brQvflqhA5K1dgcyU8nzX\/.tAImkMbRMR0ex51LjPsIk8gm0:17000:0:99999:7:::/" /tmp/mount/etc/shadow
-sed -i "s/PermitRootLogin without-password/PermitRootLogin yes/" /tmp/mount/etc/ssh/sshd_config
+sed -i -e "s/PermitRootLogin without-password/PermitRootLogin yes/
+" -e "0 i\UseDNS no
+" /tmp/mount/etc/ssh/sshd_config
 
 cp -r /mnt/os/OSMC/custom/. /tmp/mount # copy recursive include hidden ('.' not '*')
 chmod 644 /tmp/mount/etc/udev/rules.d/usbsound.rules
