93c726fbf2980a2ff6e372ac76912ab4957adc43
rern <rernrern@gmail.com>
2017-08-05 15:03:03 +0700

    Delete rebootrune.py

--- os/OSMC/custom/home/osmc/rebootrune.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import os
-
-os.system('/usr/bin/sudo /home/osmc/rebootrune.sh &')
-
-# current raspbian has no /sys/module/bcm2709/parameters/reboot_part > use noobd.conf instead
-#os.system('/usr/bin/sudo echo 8 > /sys/module/bcm2709/parameters/reboot_part; /var/www/command/rune_shutdown; reboot')
