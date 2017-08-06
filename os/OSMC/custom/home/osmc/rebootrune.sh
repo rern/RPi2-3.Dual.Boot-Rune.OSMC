72b0d71a924d02a7b37dacb409447c9e44669d8b
r e r n <r e r n>
2017-08-06 17:24:59 +0700

    remove os

--- os/OSMC/custom/home/osmc/rebootrune.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-import os
-
-os.system('/usr/bin/sudo /sbin/reboot 8')
--- os/OSMC/custom/home/osmc/soundhdmi.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-import os
-
-os.system('/usr/bin/xbmc-send -a "Notification(AUDIO OUTPUT,Switched to HDMI)"')
