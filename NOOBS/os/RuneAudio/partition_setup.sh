28622ea3b8681341e3fd7108d8188bd4494c8c35
r e r n <r e r n>
2017-08-06 17:35:44 +0700

    upload

---
+++ NOOBS/os/RuneAudio/partition_setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+mnt=/tmp/mount
+
+mkdir -p $mnt
+
+mount $part1 $mnt
+
+sed -i "s|root=/dev/[^ ]*|root=$part2|" $mnt/cmdline.txt
+
+# remove force reinstall if any
+sed -i "s| forcetrigger||" $mnt/recovery.cmdline
+umount $mnt
+
+mount $part2 $mnt
+
+sed -i -e "s|^.* /boot |$part1  /boot |
+" -e '/^#/ d
+' -e 's/\s\+0\s\+0\s*$//
+' $mnt/etc/fstab
+
+cp -r /mnt/os/RuneAudio/custom/. $mnt # customize files
+chmod 755 /tmp/mount/usr/local/bin/*reset
+
+umount $mnt
