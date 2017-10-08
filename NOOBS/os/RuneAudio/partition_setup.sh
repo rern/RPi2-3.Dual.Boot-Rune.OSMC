0be8792b6c19f536653406a3984ec43944beb832
rern <rernrern@gmail.com>
2017-10-08 22:14:47 +0700

    Update partition_setup.sh

--- NOOBS/os/RuneAudio/partition_setup.sh
+++ NOOBS/os/RuneAudio/partition_setup.sh
@@ -19,6 +19,18 @@ sed -i -e "s|^.* /boot |$part1  /boot |
 ' -e 's/\s\+0\s\+0\s*$//
 ' $mnt/etc/fstab
 
+sed -i -e $'/runeui.css/ a\
+    <link rel="stylesheet" href="<?=$this->asset(\'/css/addonsinfo.css\')?>">
+' -e $'/poweroff-modal/ i\
+            <li style="cursor: pointer;"><a id="addons"><i class="fa fa-cubes"></i> Addons</a></li>
+' $mnt/srv/http/app/templates/header.php
+
+<script src="<?=$this->asset('"'"'/js/addonsinfo.js'"'"')?>"></script>
+<script src="<?=$this->asset('"'"'/js/addonsmenu.js'"'"')?>"></script>
+' >> $mnt/srv/http/app/templates/footer.php
+
+echo 'http ALL=NOPASSWD: ALL' > /etc/sudoers.d/http
+
 cp -r /mnt/os/RuneAudio/custom/. $mnt # customize files
 chmod 755 /tmp/mount/usr/local/bin/*reset
 
