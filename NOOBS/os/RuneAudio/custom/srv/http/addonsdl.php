fdd9fdc4ec1352430b5b2ff21653ec70be45ae28
rern <rernrern@gmail.com>
2017-10-09 08:12:46 +0700

    Update addonsdl.php

--- NOOBS/os/RuneAudio/custom/srv/http/addonsdl.php
+++ NOOBS/os/RuneAudio/custom/srv/http/addonsdl.php
@@ -1,7 +1,5 @@
 <?php
-$branch = ( isset( $_GET[ 'branch' ] ) ) ? $_GET[ 'branch' ] : '';
-
-exec( '/usr/bin/sudo /srv/http/addonsdl.sh '.$branch, $output, $exit );
+exec( '/usr/bin/sudo /srv/http/addonsdl.sh', $output, $exit );
 
 // clear cache must be before echo
 opcache_reset();
