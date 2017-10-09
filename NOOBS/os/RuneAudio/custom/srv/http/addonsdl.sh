ab65c3d3c8a5339c3ae12316bf91ffe1981ce4df
rern <rernrern@gmail.com>
2017-10-09 21:03:33 +0700

    Update addonsdl.sh

--- NOOBS/os/RuneAudio/custom/srv/http/addonsdl.sh
+++ NOOBS/os/RuneAudio/custom/srv/http/addonsdl.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 dl=$( wget -qN https://github.com/rern/RuneAudio_Addons/raw/master/install.sh -P /srv/http )
-if [[ $? != 0 ]]; then
-	if [[ $? == 5 ]]; then # github 'ca certificate failed' code > update time
-		systemctl stop ntpd
-		ntpdate pool.ntp.org
-		systemctl start ntpd
-		echo "$dl"
-		[[ $? != 0 ]] && exit 5
-	else
-		exit 1
-	fi
+if [[ $? == 5 ]]; then # github 'certificate error' code
+	systemctl stop ntpd
+	ntpdate pool.ntp.org
+	systemctl start ntpd
+	echo "$dl"
+	[[ $? == 5 ]] && exit 5
 fi
+
+[[ $? != 0 ]] && exit 1
 
 chmod 755 /srv/http/install.sh
 /srv/http/install.sh
