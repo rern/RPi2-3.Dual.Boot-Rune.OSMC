e086c34d1e63cd5c149e9cf7703adeb18c7e9761
rern <rernrern@gmail.com>
2017-10-09 08:17:41 +0700

    Update addonsdl.sh

--- NOOBS/os/RuneAudio/custom/srv/http/addonsdl.sh
+++ NOOBS/os/RuneAudio/custom/srv/http/addonsdl.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-# existing 'addonslist.php'
-addonslist=$( sed -n "/'addo'/,/^),/p" /srv/http/addonslist.php )
-installurl=$( echo "$addonslist" | grep 'installurl.*=>' | cut -d "'" -f 4 )
-gitpath=$( dirname $installurl )
-branch=''
+gitpath=https://github.com/rern/RuneAudio_Addons/raw/master
 
-# for testing branch: $1=branchname
-if (( $# != 0 )); then
-	branch=$1
-	gitpath=$( echo $( dirname $gitpath )/$branch ) # switch 'master' to 'branch'
-	installurl=$gitpath/install.sh
-fi
-
-dl=$( wget -qN $gitpath/srv/http/addonslist.php -P /srv/http )
+dl=$( wget -qN https://github.com/rern/RuneAudio_Addons/raw/master/install.sh -P /srv/http )
 if [[ $? != 0 ]]; then
 	if [[ $? == 5 ]]; then # github 'ca certificate failed' code > update time
 		systemctl stop ntpd
@@ -26,19 +15,7 @@ if [[ $? != 0 ]]; then
 	fi
 fi
 
-# new 'addonslist.php'
-addonslist=$( sed -n "/'addo'/,/^),/p" /srv/http/addonslist.php )
-
-versionlist=$( echo "$addonslist" | grep 'version.*=>' | cut -d "'" -f 4 )
-versionredis=$( redis-cli hget addons addo )
-
-if [[ $versionlist != $versionredis ]]; then
-	wget -qN $installurl -P /srv/http
-	[[ $? != 0 ]] && exit 1
-	chmod 755 /srv/http/install.sh
-	
-	/usr/local/bin/uninstall_addo.sh
-	/srv/http/install.sh $branch # this line exit code = 1 ???
-fi
+chmod 755 /srv/http/install.sh
+/srv/http/install.sh
 
 exit 0
