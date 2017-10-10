e072b7f50c760c0bb3651698cb70fdda0cfc8ee6
rern <rernrern@gmail.com>
2017-10-10 19:55:07 +0700

    Add files via upload

--- NOOBS/os/RuneAudio/custom/srv/http/addonsdl.php
+++ NOOBS/os/RuneAudio/custom/srv/http/addonsdl.php
@@ -1,7 +1,4 @@
 <?php
 exec( '/usr/bin/sudo /srv/http/addonsdl.sh', $output, $exit );
 
-// clear cache must be before echo
-opcache_reset();
-
 echo $exit;
--- NOOBS/os/RuneAudio/custom/srv/http/addonsdl.sh
+++ NOOBS/os/RuneAudio/custom/srv/http/addonsdl.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-dl=$( wget -qN https://github.com/rern/RuneAudio_Addons/raw/master/install.sh -P /srv/http )
+wget -qN https://github.com/rern/RuneAudio_Addons/raw/master/install.sh -P /srv/http
 result=$?
 if [[ $result == 5 ]]; then # github 'certificate error' code
 	systemctl stop ntpd
 	ntpdate pool.ntp.org
 	systemctl start ntpd
-	echo "$dl" || exit 1
-elif [[ $result != 0 ]]; then
-	exit 1
+	wget -qN https://github.com/rern/RuneAudio_Addons/raw/master/install.sh -P /srv/http
 fi
 
 chmod 755 /srv/http/install.sh
 /srv/http/install.sh
-
-exit 0
