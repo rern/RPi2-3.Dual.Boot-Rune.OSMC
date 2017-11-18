390caf52f3b953ab8c1af1e563165059927020b8
rern <rernrern@gmail.com>
2017-11-18 12:38:45 +0700

    replace with symlink

--- NOOBS/os/RuneAudio/custom/srv/http/addonsdl.php
+++
@@ -1,9 +0,0 @@
-<?php
-$branch = ( isset( $_GET[ 'branch' ] ) ) ? $_GET[ 'branch' ] : '';
-
-exec( '/usr/bin/sudo /srv/http/addonsdl.sh '.$branch, $output, $exit );
-
-// clear cache must be before echo
-opcache_reset();
-
-echo $exit;
