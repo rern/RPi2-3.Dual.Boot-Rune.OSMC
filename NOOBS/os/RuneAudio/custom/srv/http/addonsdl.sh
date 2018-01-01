bae465eb0b5896c9986e9c00708120ae0722e3a3
rern <rernrern@gmail.com>
2018-01-01 20:25:12 +0700

    Update addonsdl.sh

--- NOOBS/os/RuneAudio/custom/srv/http/addonsdl.sh
+++ NOOBS/os/RuneAudio/custom/srv/http/addonsdl.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-wget -qN https://github.com/rern/RuneAudio_Addons/raw/master/install.sh -P /srv/http
-if [[ $? == 5 ]]; then # 'certificate error' code
-	systemctl stop ntpd
-	ntpdate pool.ntp.org
-	systemctl start ntpd
-	
-	wget -qN https://github.com/rern/RuneAudio_Addons/raw/master/install.sh -P /srv/http
-fi
+wget -qN --no-check-certificate https://github.com/rern/RuneAudio_Addons/raw/master/install.sh -P /srv/http
 
 chmod 755 /srv/http/install.sh || exit 1
 /srv/http/install.sh
