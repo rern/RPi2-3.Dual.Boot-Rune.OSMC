d730793790f0a13a93669a5c4ad24eb8cb69a6aa
rern <rernrern@gmail.com>
2018-01-09 19:22:36 +0700

    Update hardreset.sh

--- NOOBS/hardreset/hardreset.sh
+++ NOOBS/hardreset/hardreset.sh
@@ -83,8 +83,9 @@ echo $li
 echo -e "$bar $name hardreset ..."
 echo $li
 
+gitpath=https://github.com/rern/RPi2-3.Dual.Boot-Rune.OSMC/raw/master/NOOBS/hardreset
 [[ $namereset == Rune04b ]] && namereset=RuneAudio
-wget -qN --no-check-certificate --show-progress https://github.com/rern/RPi2-3.Dual.Boot-Rune.OSMC/raw/master/NOOBS/hardreset/hardreset_$namereset.sh
+wget -qN --no-check-certificate --show-progress $gitpath/hardreset_$namereset.sh
 if [[ $? != 0 ]]; then
 	echo -e '\e[38;5;7m\e[48;5;1m ! \e[0m custom files download failed.'
 	echo 'Please try again.'
@@ -100,6 +101,7 @@ mmc 1
 mmc $rootnum
 mntroot=/tmp/p$rootnum
 
+# customize
 . hardreset_$namereset.sh
 
 time1=$( date +%s )
