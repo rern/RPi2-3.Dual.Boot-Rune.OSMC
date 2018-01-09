44742e08e68ee7e7d375e7692c7331d169db68c9
rern <rernrern@gmail.com>
2018-01-09 16:19:00 +0700

    Add files via upload

--- NOOBS/os/RuneAudio/custom/usr/local/bin/hardreset_ArchLinuxArm.sh
+++ NOOBS/os/RuneAudio/custom/usr/local/bin/hardreset_ArchLinuxArm.sh
@@ -4,6 +4,6 @@ bsdtar -xvf /tmp/p1/os/$namereset/root.tar.xz -C $mntroot \
 
 # from partition_setup.sh
 echo -e "$bar Partition setup ..."
-sed -i 's/#PermitRootLogin.*/PermitRootLogin yes/' $mntroot/etc/ssh/sshd_config
 sed -i 's|root:.*|root:\$6\$X6cgc9tb\$wTTiWttk/tRwPrM8pLZCZpYpHE8zEar2mkSSQ7brQvflqhA5K1dgcyU8nzX/.tAImkMbRMR0ex51LjPsIk8gm0:17000:0:99999:7:::|
 ' $mntroot/etc/shadow
+sed -i 's/#PermitRootLogin.*/PermitRootLogin yes/' $mntroot/etc/ssh/sshd_config
