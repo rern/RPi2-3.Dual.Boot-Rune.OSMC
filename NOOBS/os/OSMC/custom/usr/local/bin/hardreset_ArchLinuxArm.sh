f5249a8bbb0ba5262d7895c6d404b397bc1eaa33
rern <rernrern@gmail.com>
2018-01-09 16:51:58 +0700

    Delete hardreset_ArchLinuxArm.sh

--- NOOBS/os/OSMC/custom/usr/local/bin/hardreset_ArchLinuxArm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-bsdtar -xvf /tmp/p1/os/$namereset/root.tar.xz -C $mntroot \
-
-# from partition_setup.sh
-echo -e "$bar Partition setup ..."
-sed -i 's|root:.*|root:\$6\$X6cgc9tb\$wTTiWttk/tRwPrM8pLZCZpYpHE8zEar2mkSSQ7brQvflqhA5K1dgcyU8nzX/.tAImkMbRMR0ex51LjPsIk8gm0:17000:0:99999:7:::|
-' $mntroot/etc/shadow
-sed -i 's/#PermitRootLogin.*/PermitRootLogin yes/' $mntroot/etc/ssh/sshd_config
