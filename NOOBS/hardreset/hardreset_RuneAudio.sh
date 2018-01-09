db537de74063ddd1fa24e13d79377bb24b9021a6
rern <rernrern@gmail.com>
2018-01-09 19:19:16 +0700

    Update hardreset_RuneAudio.sh

--- NOOBS/hardreset/hardreset_RuneAudio.sh
+++ NOOBS/hardreset/hardreset_RuneAudio.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-bsdtar -xvf /tmp/p1/os/$namereset/root.tar.xz -C $mntroot \
-  --exclude=./srv/http/.* \
-  --exclude=./usr/include \
-  --exclude=./usr/lib/{python2.7/test,python3*,libgo.*} \
-  --exclude=./usr/share/{doc,gtk-doc,info,man}
+bsdtar -xvf /tmp/p1/os/$namereset/root.tar.xz -C $mntroot
 
 # from partition_setup.sh
 echo -e "$bar Partition setup ..."
