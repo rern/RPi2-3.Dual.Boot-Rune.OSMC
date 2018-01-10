764fa00cdf3f4a974b3341f94fa17462445038e9
rern <rernrern@gmail.com>
2018-01-10 19:06:56 +0700

    Update hardreset_ArchLinuxArm.sh

--- NOOBS/hardreset/hardreset_ArchLinuxArm.sh
+++ NOOBS/hardreset/hardreset_ArchLinuxArm.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-bsdtar -xpvf /tmp/p1/os/$namereset/root.tar.xz -C $mntroot \
-
 # from partition_setup.sh
 echo -e "$bar Partition setup ..."
 sed -i 's|root:.*|root:\$6\$X6cgc9tb\$wTTiWttk/tRwPrM8pLZCZpYpHE8zEar2mkSSQ7brQvflqhA5K1dgcyU8nzX/.tAImkMbRMR0ex51LjPsIk8gm0:17000:0:99999:7:::|
