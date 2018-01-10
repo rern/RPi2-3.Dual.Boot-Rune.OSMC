bcf615a33812fc1b656fa792c2c1f2efcae36178
rern <rernrern@gmail.com>
2018-01-10 21:19:22 +0700

    Update hardreset_OSMC.sh

--- NOOBS/hardreset/hardreset_OSMC.sh
+++ NOOBS/hardreset/hardreset_OSMC.sh
@@ -2,6 +2,6 @@
 
 # from partition_setup.sh
 echo -e "$bar Partition setup ..."
-. /tmp/p1/os/$namereset/custom/usr/local/bin/custom.sh
+. $mntrecovery/os/$namereset/custom/usr/local/bin/custom.sh
 
-wget -qN --show-progress https://github.com/rern/RuneAudio/raw/master/_settings/cmd.sh -P $mntroot/etc/profile.d
+wget -qN --show-progress https://github.com/rern/OSMC/raw/master/_settings/cmd.sh -P $mntroot/etc/profile.d
