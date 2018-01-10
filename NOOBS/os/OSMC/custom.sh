3789f3438c84acd7268fa8143c5518822b56c3a2
rern <rernrern@gmail.com>
2018-01-10 21:08:30 +0700

    Update custom.sh

--- NOOBS/os/OSMC/custom.sh
+++ NOOBS/os/OSMC/custom.sh
@@ -43,7 +43,7 @@ sed -i -e 's/PermitRootLogin .*/PermitRootLogin yes/
 ' -e '/^MACs/ s/^/#/
 ' $mntroot/etc/ssh/sshd_config
 
-cp -r $mntboot/os/OSMC/custom/. $mntroot # copy recursive include hidden ('.' not '*')
+cp -r $mntrecovery/os/OSMC/custom/. $mntroot # copy recursive include hidden ('.' not '*')
 chmod 644 $mntroot/etc/udev/rules.d/usbsound.rules
 chmod 755 $mntroot/home/osmc/*.py
 chmod 755 $mntroot/usr/local/bin/hardreset*
