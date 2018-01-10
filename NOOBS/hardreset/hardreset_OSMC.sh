f11234abe019790a55d16ce4b822860e3c4f6a37
rern <rernrern@gmail.com>
2018-01-10 19:07:15 +0700

    Update hardreset_OSMC.sh

--- NOOBS/hardreset/hardreset_OSMC.sh
+++ NOOBS/hardreset/hardreset_OSMC.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-bsdtar -xpvf /tmp/p1/os/$namereset/root-rbp2.tar.xz -C $mntroot \
-  --exclude=./var/cache/apt \
-  --exclude=./usr/include \
-  --exclude=./usr/lib/{python2.7/test,python3*,libgo.*} \
-  --exclude=./usr/share/{doc,gtk-doc,info,locale,man}
-
 # from partition_setup.sh
 echo -e "$bar Partition setup ..."
 . /tmp/p1/os/$namereset/custom/usr/local/bin/custom.sh
