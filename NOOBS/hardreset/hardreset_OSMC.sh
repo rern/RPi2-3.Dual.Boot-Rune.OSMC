a75d3a0dcbbe03b854e07d08a7d9135194f4fb03
rern <rernrern@gmail.com>
2018-01-11 20:34:11 +0700

    Delete hardreset_OSMC.sh

--- NOOBS/hardreset/hardreset_OSMC.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# from partition_setup.sh
-echo -e "$bar Partition setup ..."
-. $mntrecovery/os/$namereset/custom/usr/local/bin/custom.sh
-
-wget -qN --show-progress https://github.com/rern/OSMC/raw/master/_settings/cmd.sh -P $mntroot/etc/profile.d
