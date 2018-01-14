5afc38f32391f62e591877567ffdf0bfebd1f6c8
rern <rernrern@gmail.com>
2018-01-14 20:23:26 +0700

    Update custom.sh

--- NOOBS/os/OSMC/custom.sh
+++ NOOBS/os/OSMC/custom.sh
@@ -16,24 +16,23 @@ if [[ -d /tmp/recovery ]]; then
 	part1=/dev/mmcblk0p$bootnum
 	part2=/dev/mmcblk0p$rootnum
 	echo "$part1  /boot      vfat  defaults,noatime,noauto,x-systemd.automount    0   0" > $fstab
+
+	# omit current os from installed_os.json
+	echo "
+	/dev/mmcblk0p1  /media/p1  vfat  noauto,noatime
+	/dev/mmcblk0p5  /media/p5  ext4  noauto,noatime
+	" >> $fstab
+
+	# filter boot and root partitions
+	partlist=$( fdisk -l /dev/mmcblk0 | grep mmcblk0p | awk -F' ' '{print $1}' | sed "/p1$\|p2$\|p5$\|$part1\|$part2/ d; sed s/\/dev\/mmcblk0p//" )
+	partarray=( $( echo $partlist ) )
+	ilength=${#partarray[*]}
+	for (( i=0; i < ilength; i++ )); do
+		(( $(( i % 2 )) == 0 )) && parttype=vfat || parttype=ext4
+		p=${partarray[i]}
+		echo "/dev/mmcblk0p$p  /media/p$p  $parttype  noauto,noatime" >> $fstab
+	done
 fi
-
-# omit current os from installed_os.json
-echo "
-/dev/mmcblk0p1  /media/p1  vfat  noauto,noatime
-/dev/mmcblk0p5  /media/p5  ext4  noauto,noatime
-" >> $fstab
-
-# filter boot and root partitions
-partlist=$( fdisk -l /dev/mmcblk0 | grep mmcblk0p | awk -F' ' '{print $1}' | sed "/p1$\|p2$\|p5$\|$part1\|$part2/ d; sed s/\/dev\/mmcblk0p//" )
-partarray=( $( echo $partlist ) )
-ilength=${#partarray[*]}
-for (( i=0; i < ilength; i++ )); do
-	(( $(( i % 2 )) == 0 )) && parttype=vfat || parttype=ext4
-	p=${partarray[i]}
-	echo "/dev/mmcblk0p$p  /media/p$p  $parttype  noauto,noatime" >> $fstab
-done
-
 # customize files
 sed -i 's|root:.*|root:\$6\$X6cgc9tb\$wTTiWttk/tRwPrM8pLZCZpYpHE8zEar2mkSSQ7brQvflqhA5K1dgcyU8nzX/.tAImkMbRMR0ex51LjPsIk8gm0:17000:0:99999:7:::|
 ' $mntroot/etc/shadow
