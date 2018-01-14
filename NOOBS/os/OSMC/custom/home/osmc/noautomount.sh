ba5474741a202efb03e5415a0bf4ba6066f3b4d2
rern <rernrern@gmail.com>
2018-01-14 21:07:02 +0700

    Update noautomount.sh

--- NOOBS/os/OSMC/custom/home/osmc/noautomount.sh
+++ NOOBS/os/OSMC/custom/home/osmc/noautomount.sh
@@ -7,21 +7,28 @@ mkdir /tmp/SETTINGS
 mount /dev/mmcblk0p5 /tmp/SETTINGS
 
 fstab=/etc/fstab
-echo "
+
+part1=/dev/mmcblk0p$bootnum
+part2=/dev/mmcblk0p$rootnum
+echo "$part1  /boot      vfat  defaults,noatime,noauto,x-systemd.automount    0   0
 /dev/mmcblk0p1  /media/p1  vfat  noauto,noatime
 /dev/mmcblk0p5  /media/p5  ext4  noauto,noatime
 " >> $fstab
-	
-part1=/dev/mmcblk0p$bootnum
-part2=/dev/mmcblk0p$rootnum
-echo "$part1  /boot      vfat  defaults,noatime,noauto,x-systemd.automount    0   0" > $fstab
+umount /dev/mmcblk0p1 2> /dev/null
+umount /dev/mmcblk0p5 2> /dev/null
 
 # omit current os from installed_os.json
 partlist=$( fdisk -l /dev/mmcblk0 | grep mmcblk0p | awk -F' ' '{print $1}' | sed "/p1$\|p2$\|p5$\|$part1\|$part2/ d; sed s/\/dev\/mmcblk0p//" )
 partarray=( $( echo $partlist ) )
 ilength=${#partarray[*]}
+mountlist=''
 for (( i=0; i < ilength; i++ )); do
   (( $(( i % 2 )) == 0 )) && parttype=vfat || parttype=ext4
   p=${partarray[i]}
-  echo "/dev/mmcblk0p$p  /media/p$p  $parttype  noauto,noatime" >> $fstab
+  mountlist+="/dev/mmcblk0p$p  /media/p$p  $parttype  noauto,noatime\n"
+  umount /dev/mmcblk0p$p 2> /dev/null
 done
+
+echo "mountlist" >> $fstab
+
+mount -a
