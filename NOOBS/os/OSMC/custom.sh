694931cb66bc9331b5e46ef0a325d60aaa68dd63
rern <rernrern@gmail.com>
2018-01-15 17:05:21 +0700

    Update custom.sh

--- NOOBS/os/OSMC/custom.sh
+++ NOOBS/os/OSMC/custom.sh
@@ -9,6 +9,50 @@
 # copt custom files
 # remove forcetrigger
 
+label=$( e2label /dev/sda1 )
+mnt=$mntroot/mnt/$label
+mkdir -p $mnt
+
+mountlist="/dev/sda1       $mnt   ext4  defaults,noatime
+"
+umount -l /dev/sda1
+echo
+
+echo -e "$bar Disable SD card automount ..."
+#################################################################################
+if [[ $part2 ]]; then
+	rootnum=${part2/\/dev\/mmcblk0p}
+else
+	rootnum=$( mount | grep 'on / ' | cut -d' ' -f1 | cut -d'p' -f2  )
+fi
+bootnum=$(( rootnum - 1 ))
+
+mountlist+="/dev/mmcblk0p$bootnum  /boot      vfat  defaults,noatime,noauto,x-systemd.automount    0   0
+/dev/mmcblk0p1  /media/p1  vfat  noauto,noatime
+/dev/mmcblk0p5  /media/p5  ext4  noauto,noatime
+"
+umount /dev/mmcblk0p$bootnum 2> /dev/null
+umount -l /dev/mmcblk0p1 2> /dev/null
+umount -l /dev/mmcblk0p5 2> /dev/null
+
+partlist=$( 
+	fdisk -l /dev/mmcblk0 | 
+	grep mmcblk0p | 
+	awk -F' ' '{print $1}' | 
+	sed "/p1$\|p2$\|p5$\|p$bootnum$\|p$rootnum$/ d" | 
+	sed 's/\/dev\/mmcblk0p//' 
+)
+partarray=( $( echo $partlist ) )
+ilength=${#partarray[*]}
+for (( i=0; i < ilength; i++ )); do
+  (( $(( i % 2 )) == 0 )) && parttype=vfat || parttype=ext4
+  p=${partarray[i]}
+  mountlist+="/dev/mmcblk0p$p  /media/p$p  $parttype  noauto,noatime\n"
+  umount -l /dev/mmcblk0p$p 2> /dev/null
+done
+
+echo -e "$mountlist" > $mntroot/etc/fstab
+
 # customize files
 sed -i 's|root:.*|root:\$6\$X6cgc9tb\$wTTiWttk/tRwPrM8pLZCZpYpHE8zEar2mkSSQ7brQvflqhA5K1dgcyU8nzX/.tAImkMbRMR0ex51LjPsIk8gm0:17000:0:99999:7:::|
 ' $mntroot/etc/shadow
