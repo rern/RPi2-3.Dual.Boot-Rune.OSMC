b0f9771391a897ac919afd4b67abb21b7362df99
rern <rernrern@gmail.com>
2018-02-06 07:05:42 +0700

    Update addonsdl.php

--- NOOBS/os/RuneAudio/custom/srv/http/addonsdl.php
+++ NOOBS/os/RuneAudio/custom/srv/http/addonsdl.php
@@ -1,5 +1,5 @@
 <?php
-exec( '/usr/bin/sudo /srv/http/addonsdl.sh', $output, $exit ); // no sudo here
+exec( '/usr/bin/sudo /srv/http/addonsdl.sh', $output, $exit );
 // clear cache must be before echo
 opcache_reset();
 echo $exit;
