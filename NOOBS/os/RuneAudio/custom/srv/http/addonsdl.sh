51b07d97c759d25a5d687090a3056fe9d69ffb4f
rern <rernrern@gmail.com>
2018-02-07 08:17:26 +0700

    Update addonsdl.sh

--- NOOBS/os/RuneAudio/custom/srv/http/addonsdl.sh
+++ NOOBS/os/RuneAudio/custom/srv/http/addonsdl.sh
@@ -2,5 +2,5 @@
 
 wget -qN --no-check-certificate https://github.com/rern/RuneAudio_Addons/raw/master/install.sh -P /srv/http
 
-chmod 755 /srv/http/install.sh || exit 1
+chmod 755 /srv/http/install.sh || exit 8
 /srv/http/install.sh
